9db5b5ce3e435b3ea58a9c94edbb4a7003de8873
abhaysp95 <abhaysp95@protonmail.com>
2024-06-21 09:46:22 +0530

    feat: optimized solution using binary search

--- leetcode/daily_challenges/1552._Magnetic_Force_Between_Two_Balls.py
+++ leetcode/daily_challenges/1552._Magnetic_Force_Between_Two_Balls.py
@@ -1,4 +1,41 @@
+# Following question is based on: https://www.spoj.com/problems/AGGRCOW/ 
+
 class Solution(object):
+    def maxDistance(self, position: list[int], m: int) -> int:
+        """
+        :type n: int
+        :type quantities: List[int]
+        :rtype: int
+        """
+        position.sort() 
+
+        low = 1
+        high = position[-1] - position[0]
+        ans = 1
+        while low <= high:
+            mid = low + ((high - low) // 2)
+            placed = self.canPlace(position, m, mid)
+            if placed:
+                ans = mid
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return ans
+    
+    def canPlace(self, position: list[int], m: int, d: int) -> bool:
+        placed = 1
+        lastPlaced = position[0]
+        for i in range(1, len(position)):
+            if position[i] - lastPlaced >= d:
+                lastPlaced = position[i]
+                placed += 1
+            if placed >= m:
+                return True
+        return False
+
+# brute force solution
+class SolutionBruteForce(object):
     def maxDistance(self, position: list[int], m: int) -> int:
         position.sort()
         d = 1
@@ -24,10 +61,14 @@ class Solution(object):
         return min(diff_list)
 
 
-str = input()[:-1]
-str = str[1:]
-position = [int(x) for x in str.split(',')]
-m = int(input())
+def main() -> None:
+    str = input()[:-1]
+    str = str[1:]
+    position = [int(x) for x in str.split(',')]
+    m = int(input())
 
-solve = Solution()
-print(solve.maxDistance(position, m))
+    solve = SolutionBruteForce()
+    print(solve.maxDistance(position, m))
+
+if __name__ == "__main__":
+    main()
