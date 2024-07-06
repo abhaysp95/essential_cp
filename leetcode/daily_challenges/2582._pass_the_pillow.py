031843bd2605461d44e19db00e1691db3af27795
abhaysp95 <abhaysp95@protonmail.com>
2024-07-06 18:34:04 +0530

    feat: add loop based solution in addition to math based solution

--- leetcode/daily_challenges/2582._pass_the_pillow.py
+++ leetcode/daily_challenges/2582._pass_the_pillow.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 
-class Solution:
+from abc import ABCMeta, abstractmethod
+
+class Solution(metaclass=ABCMeta):
+    @abstractmethod
+    def passThePillow(self, n: int, time: int) -> int:
+        return NotImplementedError
+
+class SolutionMathBased(Solution):
     def passThePillow(self, n: int, time: int) -> int:
         if time < n:
             return time + 1
@@ -13,13 +20,26 @@ class Solution:
         else:
             return time + 1
 
+class SolutionLoopBased(Solution):
+    def passThePillow(self, n: int, time: int) -> int:
+        idx, cidx, dir = 1, 1, 1
+        while idx <= time:
+            if dir == 1:
+                cidx += 1
+            else:
+                cidx -= 1
+            idx += 1
+            if cidx == n:
+                dir *= -1
+        return cidx
+
 def main():
     line = input().split(" ")
     # first input n
     n = int(line[0])
     # second input time
     time = int(line[1])
-    solve = Solution()
+    solve = SolutionLoopBased()
     print(solve.passThePillow(n, time))
 
 if __name__ == "__main__":
