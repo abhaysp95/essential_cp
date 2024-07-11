8f5e41eb3c12729289c167d6dfde1908a33fb2b3
abhaysp95 <abhaysp95@protonmail.com>
2024-07-11 12:57:10 +0530

    feat: daily leetcode challenge for 2024-07-11
        introducing in-place reverse function
        strangely leetcode is showing more runtime for this

--- leetcode/daily_challenges/1190._reverse_substrings_between_each_pair_of_parentheses.py
+++ leetcode/daily_challenges/1190._reverse_substrings_between_each_pair_of_parentheses.py
@@ -9,12 +9,22 @@ class Solution:
         new_str = []
         ca = 0 # count for alphabet only
 
+        def rev_string(s: List[str], lo: int, hi: int):
+            print(f"{s}, {lo}, {hi}")
+            half = (hi - lo) // 2
+            print(f"half: {half}")
+            for i in range(half):
+                temp = s[hi - i - 1]
+                s[hi - i - 1] = s[lo + i]
+                s[lo + i] = temp
+
         for c in s:
             if c == "(":
                 stack.append(ca)
             elif c == ")":
                 prev = stack.pop()
-                new_str[prev:ca] = new_str[prev:ca][::-1] # reverse the list
+                # new_str[prev:ca] = new_str[prev:ca][::-1] # reverse the list
+                rev_string(new_str, prev, ca)
             else: # getting alphabet here
                 new_str.append(c)
                 ca += 1
