1f96fd7a3fd42298046cb97af8306600c7e1c40d
abhaysp95 <abhaysp95@protonmail.com>
2024-08-12 00:05:23 +0530

    feat: cses mathematics counting divisors
      - solving more effeciently by doing prime factorization, but it's not
        efficient yet. Will update with efficient method

--- cses/mathematics/counting_divisors.cpp
+++ cses/mathematics/counting_divisors.cpp
@@ -24,13 +24,33 @@ int divisor_pairs(int n) {
   return c;
 }
 
+int by_prime_factorization(int n) {
+  int c=1; 
+  for (int i=2; i*i<=n; i++) {
+    int tc=0;
+    while (n%i==0) {
+      n/=i;
+      tc++;
+    }
+    if (tc) c*=tc+1;
+  }
+
+  // NOTE: here if we have i=n>1, then number i is also a divisor and so will be
+  // n/i (divisor pair). Thus we need to multiply by 2
+  // This is very same to how we needed to check for n>1 in distinct prime
+  // factors and added +1 when n>1 was true
+  if (n>1) c*=2;
+
+  return c;
+}
+
 int main() {
   int t;
   scanf("%d", &t);
   while (t--) {
     int n;
     scanf("%d", &n);
-    int c = divisor_pairs(n);
+    int c = by_prime_factorization(n);
     printf("%d\n",c);
   }
 }
