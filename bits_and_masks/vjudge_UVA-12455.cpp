05640e51efb9840e38f8ca36df430b9e1582814a
abhaysp95 <abhaysp95@protonmail.com>
2024-08-21 23:04:33 +0530

    feat: fixed solution for easy bitmask concept problem

--- bits_and_masks/vjudge_UVA-12455.cpp
+++ bits_and_masks/vjudge_UVA-12455.cpp
@@ -4,36 +4,37 @@
 
 using namespace std;
 
+void solve() {
+  int n,p;
+  scanf("%d %d", &n, &p);
+  int bars[p];
+  for (int i=0; i<p; i++)
+    scanf("%d", &bars[i]);
+
+  bool stat=false;
+
+  for (int mask=0; mask<(1<<p); mask++) {
+    int j=0;
+    int sum=0;
+    while (j<p) {
+      if (mask&(1<<j)) sum+=bars[j];
+      j++;
+    }
+    if (sum==n) {
+      stat=true;
+      break;
+    }
+  }
+
+  printf("%s\n", stat||n==0?"YES":"NO");
+}
+
 int main(void) {
   int t;
   scanf("%d", &t);
   while (t--) {
-    int n,p;
-    scanf("%d %d", &n, &p);
-    int bars[p];
-    for (int i=0; i<p; i++)
-      scanf("%d", &bars[i]);
-
-    if (n==0) {
-      printf("YES\n");
-      return 0;
-    }
-
-    for (int mask=0; mask<=(1<<p); mask++) {
-      int j=0;
-      int sum=0;
-      while (1<<j<mask) {
-        if (mask&(1<<j)) sum+=bars[j];
-        j++;
-      }
-      if (sum==n) {
-        printf("YES\n");
-        return 0;
-      }
-    }
+    solve();
   }
-
-  printf("NO\n");
 
   return 0;
 }
